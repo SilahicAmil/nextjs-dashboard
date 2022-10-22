86d19aa9e92de889a09a51e91b0ecfac7ac5a280
Amil Silahic <jinobie02@gmail.com>
2022-10-22 10:57:31 -0600

    added dropdown menu again, "finalized" layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,34 @@
+import TicketTable from "../components/TicketTable/TicketTable";
+
+const DUMMY_DATA = [
+  {
+    id: "t1",
+    owner: "John Stossel",
+    tier: "Tier A",
+    title: "Fix useMemo Hook",
+  },
+  {
+    id: "t2",
+    owner: "Steven Stossel",
+    tier: "Tier B",
+    title: "Fix Form Hook",
+  },
+  {
+    id: "t3",
+    owner: "John Stevens",
+    tier: "Tier AA",
+    title: "Fix useEffect Hook",
+  },
+];
+
 const HomePage = () => {
   return (
     <>
       <div>
-        <h1>Home Page</h1>
+        <h1 className="text-2xl p-4">Home Page</h1>
+
+        {/* Add a  */}
+        <TicketTable data={DUMMY_DATA} />
       </div>
     </>
   );

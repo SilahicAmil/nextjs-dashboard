a1e6207bcdfc26b22fae04594e4368ad7d934b39
Amil Silahic <jinobie02@gmail.com>
2022-10-28 11:15:29 -0600

    added new ticket form

--- components/NewTicket/NewTicket.js
+++ components/NewTicket/NewTicket.js
@@ -5,42 +5,85 @@ const NewTicket = () => {
 
   return (
     <>
-      <div>
-        <form onSubmit={submitFormHandler}>
-          <label htmlFor="Ticket Title">Ticket Title </label>
-          <input type="text" className="form-input text-black" />
-          <label htmlFor="Ticket Tier" className="form-label">
-            Ticket Tier:
-          </label>
-          <select
-            name=""
-            id=""
-            className="form-select  rounded-full text-black"
-          >
-            <option value="Tier C" className="form-option ">
-              Tier C
-            </option>
-            <option value="Tier B" className="form-option ">
-              Tier B
-            </option>
-            <option value="Tier A" className="form-option ">
-              Tier A
-            </option>
-          </select>
-          <textarea
-            name=""
-            id=""
-            cols="35"
-            rows="5"
-            className="form-textarea"
-          ></textarea>
-          <select name="" id="" className="text-black rounded-full py-2">
-            <option value="Web">Web</option>
-            <option value="Web">Mobile</option>
-          </select>
-          <button type="submit" className="btn bg-red-500 text-white">
-            Submit
-          </button>
+      <div className="mt-4 h-full bg-base-100 flex items-center justify-center">
+        <form className="w-full max-w-lg ">
+          <div className="flex flex-wrap -mx-3 mb-6 ">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-xs font-bold mb-2"
+                for="grid-first-name"
+              >
+                Ticket Title:
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-first-name"
+                type="text"
+                placeholder="Title"
+              />
+              {/* <p className="text-red-500 text-xs italic">
+                Please fill out this field.
+              </p> */}
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide  text-xs font-bold mb-2"
+                for="grid-ticket-tier"
+              >
+                Tier:
+              </label>
+              <select
+                id="grid-dropdown"
+                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              >
+                <option value="">Tier A</option>
+                <option value="">Tier B</option>
+                <option value="">Tier C</option>
+              </select>
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                for="grid-password"
+              >
+                Ticket Description:
+              </label>
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+                for="grid-state"
+              >
+                Platform
+              </label>
+              <div className="relative">
+                <select
+                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-state"
+                >
+                  <option>Web</option>
+                  <option>Mobile</option>
+                </select>
+              </div>
+            </div>
+            <div className="relative md:w-1/3 px-3 mb-6 md:mb-0 m-7">
+              <input type="file" className="tracking-wide block " />
+            </div>
+            <button className="btn block tracking-wide w-full bg-white text-black mt-4">
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </>
